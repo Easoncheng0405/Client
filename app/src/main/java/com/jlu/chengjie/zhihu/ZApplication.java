a2883f1a4a1597ccfc346186125017f5f86ca164
Easoncheng0405 <597021782@qq.com>
2019-02-22 21:27:25 +0800

    [0.0.1] Log Util
    Create Custom Log Util: ZLog
    
    Signed-off-by: chengjie <chengjie.jlu@qq.com>

--- app/src/main/java/com/jlu/chengjie/zhihu/ZApplication.java
+++ app/src/main/java/com/jlu/chengjie/zhihu/ZApplication.java
@@ -1,9 +1,28 @@
+/*
+ *    Copyright [2019] [chengjie.jlu@qq.com]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.jlu.chengjie.zhihu;
 
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jlu.chengjie.zhihu.util.ZLog;
 
 import java.lang.ref.WeakReference;
 
@@ -13,6 +32,8 @@ public class ZApplication extends Application {
     private static WeakReference<Context> contextHolder;
 
     private static WeakReference<Activity> currentActivity;
+
+    private static final String TAG = "ZApplication";
 
     @Override
     public void onCreate() {
@@ -25,36 +46,37 @@ public class ZApplication extends Application {
 
         @Override
         public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
+            ZLog.d(TAG, "activity onCreate: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityStarted(Activity activity) {
-
+            ZLog.d(TAG, "activity onStart: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityResumed(Activity activity) {
-
+            ZLog.d(TAG, "activity onResume: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityPaused(Activity activity) {
-
+            ZLog.d(TAG, "activity onPause: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityStopped(Activity activity) {
-
+            ZLog.d(TAG, "activity onStop: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
+            ZLog.d(TAG, "activity onSaveInstanceState: " + activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityDestroyed(Activity activity) {
+            ZLog.d(TAG, "activity onDestroyed: " + activity.getClass().getSimpleName());
         }
     };
 
@@ -68,6 +90,7 @@ public class ZApplication extends Application {
 
     @Override
     protected void finalize() throws Throwable {
+        Log.d(TAG, "Application exit");
         super.finalize();
     }
 
---
+++ app/src/main/java/com/jlu/chengjie/zhihu/util/ZLog.java
@@ -0,0 +1,47 @@
+/*
+ *    Copyright [2019] [chengjie.jlu@qq.com]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.jlu.chengjie.zhihu.util;
+
+import android.util.Log;
+
+@SuppressWarnings("unused")
+public class ZLog {
+
+    private static final String PRE_FIX = "ZhiHu-";
+
+    private static boolean debug = true;
+
+    private static String addPreFix(String tag) {
+        return PRE_FIX + tag;
+    }
+
+    public static void d(String tag, String msg) {
+        if (debug) Log.d(addPreFix(tag), msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (debug) Log.w(addPreFix(tag), msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (debug) Log.e(addPreFix(tag), msg);
+    }
+
+    public static void setDebug(boolean on) {
+        debug = on;
+    }
+}
